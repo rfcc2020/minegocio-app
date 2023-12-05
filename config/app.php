2bf7048bc3bdb7e40b0f1a68f34588bd2a0cde1b
Robinson Cuzco <40906223+rfcc2020@users.noreply.github.com>
2023-12-05 17:46:10 -0500

    elimina .env y url en appconfig

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://minegocioapp.azurewebsites.net/public'),
 
     'asset_url' => env('ASSET_URL'),
 
