2a5ae929eda0e2c0a552f54db4bf7af9f7c02ece
Robinson Cuzco <40906223+rfcc2020@users.noreply.github.com>
2023-12-09 13:26:45 -0500

    localhost

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'MiNegocio'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', ''),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
